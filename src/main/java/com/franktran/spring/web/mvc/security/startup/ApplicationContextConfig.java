f8e2455860f41d102566d091d0f5f8fb9863ff01
frank <franktranvantu@gmail.com>
2021-06-27 00:24:46 +0700

    Building a Hello World spring MVC web app

---
+++ src/main/java/com/franktran/spring/web/mvc/security/startup/ApplicationContextConfig.java
@@ -0,0 +1,22 @@
+package com.franktran.spring.web.mvc.security.startup;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "com.franktran.spring.web.mvc.security")
+public class ApplicationContextConfig {
+
+  @Bean
+  public ViewResolver viewResolver() {
+    InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+    viewResolver.setPrefix("/WEB-INF/views/");
+    viewResolver.setSuffix(".jsp");
+    return viewResolver;
+  }
+}
---
+++ src/main/java/com/franktran/spring/web/mvc/security/startup/RootConfig.java
@@ -0,0 +1,4 @@
+package com.franktran.spring.web.mvc.security.startup;
+
+public class RootConfig {
+}
